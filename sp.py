4bd24a94fdc3a9688790b5e409c7bd12ff506e6e
Amr khaled <47401454+0x37U@users.noreply.github.com>
2020-08-07 14:26:18 +0200

    Update sp.py

--- sp.py
+++ sp.py
@@ -1,9 +1,9 @@
-print("\t[+] Separate the words on one line [+]")
-text = input("Enter ur list : ")
-def Sep():
-  readd = open(text,'r').read()
-  chng = readd.replace(" ","\n")
-  with open ('words.txt','a+') as f:
-    f.write(chng)
-  print(chng)
-Sep()
+print("\t[+] Separate the words on one line [+]") #Title name tool
+text = input("Enter ur list : ") #input for ask u about list
+def Sep(): #function
+  readd = open(text,'r').read() #open and read text file
+  chng = readd.replace(" ","\n") #Replace Space to new line
+  with open ('words.txt','a+') as f: # open new file for put Separate words
+    f.write(chng) # write in new file
+  print(chng) #output Separate
+Sep() #close function
